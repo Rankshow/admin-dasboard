a3dd78cdfbfdc5ad7be37900d9ec82812500d3e9
Rankshow <uwaomaobinna20@gmail.com>
2023-08-04 13:05:57 +0100

    worked on homepage

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='container-fluid'>
       <div className="row">
-        <div className="d-flex justify-content-around align-items-center col-12 col-sm-6 col-lg-3 bg- border-secondary">
+        <div className="d-flex bg-danger m-3 p-4 justify-content-around align-items-center col-12 col-sm-6 col-lg-3 bg- border-secondary">
           <i className='bi bi-coin fs-1'></i>
             <div>
               <p>Finance</p>
